f51203d9523c930cba03428f4947e19c25141df9
george <george@test.com>
2015-11-17 14:37:55 +0200

    test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hiiiiiiiiiiiiiiiiiiii");
             Console.WriteLine("Intro to Git");
             Console.WriteLine("--------------------------------------");
             Console.WriteLine("Hello there, this is my first Git Test");
